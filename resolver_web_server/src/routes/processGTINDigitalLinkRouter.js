0944abf18e33ee476d1ab27e5f8d9bd3f8475cbe
Nam Hoang <hoangxuannam160493@gmail.com>
2023-11-01 16:40:23 +0700

    feat: define endpoint for GTIN epcis transformation event
    Signed-off-by: Nam Hoang <hoangxuannam160493@gmail.com>

--- resolver_web_server/src/routes/processGTINDigitalLinkRouter.js
+++ resolver_web_server/src/routes/processGTINDigitalLinkRouter.js
@@ -4,13 +4,32 @@
  */
 const express = require('express');
 const processStartTime = require('../middleware/processStartTime');
-const { identificationKeyController } = require('../controllers/processDigitalLinkController');
+const {
+  identificationKeyController,
+} = require('../controllers/processDigitalLinkController');
 
 const router = express.Router();
 
-router.route('/:identificationKey').get(processStartTime, identificationKeyController);
-router.route('/:identificationKey/:lotnumber/:lotnumberkey').get(processStartTime, identificationKeyController);
-router.route('/:identificationKey/:lotnumber/:lotnumberkey/:serialnumber/:serialnumbervalue').get(processStartTime, identificationKeyController);
-router.route('/:identificationKey/:cpv/:cpvkey/:lotnumber/:lotnumberkey/:serialnumber/:serialnumbervalue').get(processStartTime, identificationKeyController);
+router
+  .route('/:identificationKey')
+  .get(processStartTime, identificationKeyController);
+router
+  .route('/:identificationKey/:lotnumber/:lotnumberkey')
+  .get(processStartTime, identificationKeyController);
+router
+  .route(
+    '/:identificationKey/:lotnumber/:lotnumberkey/:serialnumber/:serialnumbervalue'
+  )
+  .get(processStartTime, identificationKeyController);
+router
+  .route(
+    '/:identificationKey/:cpv/:cpvkey/:lotnumber/:lotnumberkey/:serialnumber/:serialnumbervalue'
+  )
+  .get(processStartTime, identificationKeyController);
+router
+  .route(
+    '/:identificationKey/3101/:netweight/13/:packdate/:serialnumber/:serialnumbervalue'
+  )
+  .get(processStartTime, identificationKeyController);
 
 module.exports = router;
